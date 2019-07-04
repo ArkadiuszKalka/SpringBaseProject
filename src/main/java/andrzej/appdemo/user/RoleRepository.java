849ec039fc9c2c7c0c8a0485083ac7a87fe57868
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-04 16:08:00 +0200

    Dodano klase UserServiceImpl

--- src/main/java/andrzej/appdemo/user/RoleRepository.java
+++ src/main/java/andrzej/appdemo/user/RoleRepository.java
@@ -3,7 +3,7 @@ package andrzej.appdemo.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository("roleRepository")
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
     public Role findByRole(String role);
--- src/main/java/andrzej/appdemo/user/UserRepository.java
+++ src/main/java/andrzej/appdemo/user/UserRepository.java
@@ -3,7 +3,7 @@ package andrzej.appdemo.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository("userRepository")
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     public User findByEmail(String email);
 
