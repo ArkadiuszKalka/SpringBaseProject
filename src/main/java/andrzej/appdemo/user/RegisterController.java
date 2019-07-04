d6c5ef468d24565154b75b52968295a78771d63f
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-04 16:35:02 +0200

    Edytowano klase RegisterController

--- src/main/java/andrzej/appdemo/user/RegisterController.java
+++ src/main/java/andrzej/appdemo/user/RegisterController.java
@@ -1,13 +1,27 @@
 package andrzej.appdemo.user;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 
+import andrzej.appdemo.validators.UserRegisterValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Locale;
 
 @Controller
 public class RegisterController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    MessageSource messageSource;
+
 
     @GET
     @RequestMapping(value = "/register")
@@ -17,4 +31,29 @@ public class RegisterController {
         return "register";
     }
 
+    @POST
+    @RequestMapping(value = "/adduser")
+    public String registerAction(User user, BindingResult result, Model model, Locale locale) {
+        String returnPage = null;
+        User userExist = userService.findUserByEmail(user.getEmail());
+
+        new UserRegisterValidator().validate(user, result);
+
+        if (userExist != null) {
+            result.rejectValue("email", messageSource.getMessage("error.userEmailExist", null, locale));
+        }
+
+        if (result.hasErrors()) {
+            returnPage = "register";
+        } else {
+            userService.saveUser(user);
+            model.addAttribute("message", messageSource.getMessage("user.service.success", null, locale));
+            model.addAttribute("user", new User());
+            returnPage = "register";
+        }
+
+        return returnPage;
+    }
 }
+
+
