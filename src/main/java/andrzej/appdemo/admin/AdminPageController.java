60ba68381397402c8c49cce3102d165ebb37729c
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-09 13:57:01 +0200

    Poprawka w user.jsp - wyświetlanie numerów rekordów

--- src/main/java/andrzej/appdemo/admin/AdminPageController.java
+++ src/main/java/andrzej/appdemo/admin/AdminPageController.java
@@ -17,6 +17,7 @@ import andrzej.appdemo.user.User;
 
 @Controller
 public class AdminPageController {
+    private static int ELEMENTS = 4;
 
     @Autowired
     private AdminService adminService;
@@ -37,15 +38,15 @@ public class AdminPageController {
         int currentPage = pages.getNumber();
         List<User> userList = pages.getContent();
         model.addAttribute("totalPages", totalPages);
-        model.addAttribute("currentPage", currentPage+1);
+        model.addAttribute("currentPage", currentPage + 1);
         model.addAttribute("userList", userList);
+        model.addAttribute("recordStartCounter", currentPage * ELEMENTS);
         return "admin/users";
     }
 
 
     private Page<User> getAllUserPagable(int page) {
-        int elements = 5;
-        Page<User> pages = adminService.findAll(PageRequest.of(page, elements));
+        Page<User> pages = adminService.findAll(PageRequest.of(page, ELEMENTS));
         for (User users : pages) {
             int numerRoli = users.getRoles().iterator().next().getId();
             users.setNrRoli(numerRoli);
