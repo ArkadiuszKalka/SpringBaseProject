606ce686efad4260a23359ed8ed0fa492d25a650
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-09 14:28:06 +0200

    Modyfikacja Admin repo, service i serviceImpl

--- src/main/java/andrzej/appdemo/admin/AdminService.java
+++ src/main/java/andrzej/appdemo/admin/AdminService.java
@@ -1,11 +1,14 @@
 package andrzej.appdemo.admin;
 
-import andrzej.appdemo.user.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface AdminService {
+import andrzej.appdemo.user.User;
 
+public interface AdminService {
+	
 	Page<User> findAll(Pageable pageable);
+	User findUserById(int id);
+	void updateUser(int id, int nrRoli, int activity);
 
 }
--- src/main/java/andrzej/appdemo/admin/AdminServiceImpl.java
+++ src/main/java/andrzej/appdemo/admin/AdminServiceImpl.java
@@ -1,6 +1,5 @@
 package andrzej.appdemo.admin;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,11 +14,25 @@ public class AdminServiceImpl implements AdminService {
 	
 	@Autowired
 	private AdminRepository adminRepository;
-	
+
 	@Override
 	public Page<User> findAll(Pageable pageable) {
 		Page<User> userList = adminRepository.findAll(pageable);
 		return userList;
 	}
+
+	@Override
+	public User findUserById(int id) {
+		User user = adminRepository.findUserById(id);
+		return user;
+	}
+
+	@Override
+	public void updateUser(int id, int nrRoli, int activity) {
+		adminRepository.updateActivationUser(activity, id);
+		adminRepository.updateRoleUser(nrRoli, id);
+	}
+
+	
 
 }
