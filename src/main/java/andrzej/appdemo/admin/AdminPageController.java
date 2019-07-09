e8327dbfc29fee4f80664fdd388afe59858e6fec
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-09 13:04:19 +0200

    Modyfikacja metody openAdminAllUsersPage i dodanie getAllUserPagable - właczenie paginacji dla wyświetlenia listy userów

--- src/main/java/andrzej/appdemo/admin/AdminPageController.java
+++ src/main/java/andrzej/appdemo/admin/AdminPageController.java
@@ -5,42 +5,51 @@ import java.util.List;
 import javax.ws.rs.GET;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import andrzej.appdemo.user.User;
 
 @Controller
 public class AdminPageController {
-	
-	@Autowired
-	private AdminService adminService;
-	
-	@GET
-	@RequestMapping(value = "/admin")
-	@Secured(value = {"ROLE_ADMIN"})
-	public String openAdminMainPage() {
-		return "admin/admin";
-	}
 
-	@GET
-	@RequestMapping(value = "/admin/users")
-	@Secured(value = {"ROLE_ADMIN"})
-	public String openAdminAllUsersPage(Model model) {
-		List<User> userList = getAllUsers();
-		model.addAttribute("userList", userList);
-		return "admin/users";
-	}
-	
-	//Pobranie listy userów
-	private List<User> getAllUsers() {
-		List<User> usersList = adminService.findAll();
-		for(User users : usersList) {
-			int numerRoli = users.getRoles().iterator().next().getId();
-			users.setNrRoli(numerRoli);
-		}
-		return usersList;
-	}
+    @Autowired
+    private AdminService adminService;
+
+    @GET
+    @RequestMapping(value = "/admin")
+    @Secured(value = {"ROLE_ADMIN"})
+    public String openAdminMainPage() {
+        return "admin/admin";
+    }
+
+    @GET
+    @RequestMapping(value = "/admin/users/{page}")
+    @Secured(value = {"ROLE_ADMIN"})
+    public String openAdminAllUsersPage(@PathVariable("page") int page, Model model) {
+        Page<User> pages = getAllUserPagable(page - 1);
+        int totalPages = pages.getTotalPages();
+        int currentPage = pages.getNumber();
+        List<User> userList = pages.getContent();
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("currentPage", currentPage+1);
+        model.addAttribute("userList", userList);
+        return "admin/users";
+    }
+
+
+    private Page<User> getAllUserPagable(int page) {
+        int elements = 5;
+        Page<User> pages = adminService.findAll(PageRequest.of(page, elements));
+        for (User users : pages) {
+            int numerRoli = users.getRoles().iterator().next().getId();
+            users.setNrRoli(numerRoli);
+        }
+        return pages;
+    }
 }
