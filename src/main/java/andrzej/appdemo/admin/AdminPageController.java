0fc721c52cbcc3fd0ef7c52c0b86d79e2ed54db2
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-12 14:57:46 +0200

    Dodano metodę do obsługi wyszukiwania userów openSearchUserspage w AdminPageController

--- src/main/java/andrzej/appdemo/admin/AdminPageController.java
+++ src/main/java/andrzej/appdemo/admin/AdminPageController.java
@@ -23,95 +23,109 @@ import andrzej.appdemo.user.User;
 @Controller
 public class AdminPageController {
 
-	private static int ELEMENTS = 10;
+    private static int ELEMENTS = 10;
 
-	@Autowired
-	private AdminService adminService;
-	
-	@Autowired
-	private MessageSource messageSource;
+    @Autowired
+    private AdminService adminService;
 
-	@GET
-	@RequestMapping(value = "/admin")
-	@Secured(value = { "ROLE_ADMIN" })
-	public String openAdminMainPage() {
-		return "admin/admin";
-	}
+    @Autowired
+    private MessageSource messageSource;
 
-	@GET
-	@RequestMapping(value = "/admin/users/{page}")
-	@Secured(value = { "ROLE_ADMIN" })
-	public String openAdminAllUsersPage(@PathVariable("page") int page, Model model) {
-		Page<User> pages = getAllUsersPageable(page - 1);
-		int totalPages = pages.getTotalPages();
-		int currentPage = pages.getNumber();
-		List<User> userList = pages.getContent();
-		model.addAttribute("totalPages", totalPages);
-		model.addAttribute("currentPage", currentPage + 1);
-		model.addAttribute("userList", userList);
-		model.addAttribute("recordStartCounter", currentPage * ELEMENTS);
-		return "admin/users";
-	}
-	
-	@GET
-	@RequestMapping(value = "/admin/users/edit/{id}")
-	@Secured(value = { "ROLE_ADMIN" })
-	public String getUserToEdit(@PathVariable("id") int id, Model model) {
+    @GET
+    @RequestMapping(value = "/admin")
+    @Secured(value = {"ROLE_ADMIN"})
+    public String openAdminMainPage() {
+        return "admin/admin";
+    }
 
-		User user = new User();
-		user = adminService.findUserById(id);
+    @GET
+    @RequestMapping(value = "/admin/users/{page}")
+    @Secured(value = {"ROLE_ADMIN"})
+    public String openAdminAllUsersPage(@PathVariable("page") int page, Model model) {
+        Page<User> pages = getAllUsersPageable(page - 1);
+        int totalPages = pages.getTotalPages();
+        int currentPage = pages.getNumber();
+        List<User> userList = pages.getContent();
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("currentPage", currentPage + 1);
+        model.addAttribute("userList", userList);
+        model.addAttribute("recordStartCounter", currentPage * ELEMENTS);
+        return "admin/users";
+    }
 
-		Map<Integer, String> roleMap = new HashMap<Integer, String>();
-		roleMap = prepareRoleMap();
-				
-		Map<Integer, String> activityMap = new HashMap<Integer, String>();
-		activityMap = prepareActivityMap();
-		
-		int rola = user.getRoles().iterator().next().getId();
-		user.setNrRoli(rola);
-	
-		model.addAttribute("roleMap", roleMap);
-		model.addAttribute("activityMap", activityMap);
-		model.addAttribute("user", user);
+    @GET
+    @RequestMapping(value = "/admin/users/edit/{id}")
+    @Secured(value = {"ROLE_ADMIN"})
+    public String getUserToEdit(@PathVariable("id") int id, Model model) {
 
-		return "admin/useredit";
-	}
-	
-	@POST
-	@RequestMapping(value = "/admin/updateuser/{id}")
-	@Secured(value = "ROLE_ADMIN")
-	public String updateUser(@PathVariable("id") int id, User user) {
-		int nrRoli = user.getNrRoli();
-		int czyActive = user.getActive();
-		adminService.updateUser(id, nrRoli, czyActive);
-		return "redirect:/admin/users/1";
-	}
+        User user = new User();
+        user = adminService.findUserById(id);
 
-	// Pobranie listy userów
-	private Page<User> getAllUsersPageable(int page) {
-		Page<User> pages = adminService.findAll(PageRequest.of(page, ELEMENTS));
-		for (User users : pages) {
-			int numerRoli = users.getRoles().iterator().next().getId();
-			users.setNrRoli(numerRoli);
-		}
-		return pages;
-	}
+        Map<Integer, String> roleMap = new HashMap<Integer, String>();
+        roleMap = prepareRoleMap();
 
-	// przygotowanie mapy ról
-	public Map<Integer, String> prepareRoleMap() {
-		Locale locale = Locale.getDefault();
-		Map<Integer, String> roleMap = new HashMap<Integer, String>();
-		roleMap.put(1, messageSource.getMessage("word.admin", null, locale));
-		roleMap.put(2, messageSource.getMessage("word.user", null, locale));
-		return roleMap;
-	}
-	
-	// przygotowanie may aktywny/nieaktywny
-	public Map<Integer, String> prepareActivityMap() {
-		Locale locale = Locale.getDefault();
-		Map<Integer, String> activityMap = new HashMap<Integer, String>();
-		activityMap.put(0, messageSource.getMessage("word.nie", null, locale));
-		activityMap.put(1, messageSource.getMessage("word.tak", null, locale));
-		return activityMap;
-	}
+        Map<Integer, String> activityMap = new HashMap<Integer, String>();
+        activityMap = prepareActivityMap();
+
+        int rola = user.getRoles().iterator().next().getId();
+        user.setNrRoli(rola);
+
+        model.addAttribute("roleMap", roleMap);
+        model.addAttribute("activityMap", activityMap);
+        model.addAttribute("user", user);
+
+        return "admin/useredit";
+    }
+
+    @POST
+    @RequestMapping(value = "/admin/updateuser/{id}")
+    @Secured(value = "ROLE_ADMIN")
+    public String updateUser(@PathVariable("id") int id, User user) {
+        int nrRoli = user.getNrRoli();
+        int czyActive = user.getActive();
+        adminService.updateUser(id, nrRoli, czyActive);
+        return "redirect:/admin/users/1";
+    }
+
+    @GET
+    @RequestMapping(value = "/admin/users/search/{searchWord}")
+    @Secured(value = "ROLE_ADMIN")
+    public String openSearchUsersPage(@PathVariable("searchWord") String searchWord, Model model) {
+        List<User> userList = adminService.findAllSearch(searchWord);
+        for (User users : userList) {
+            int numerRoli = users.getRoles().iterator().next().getId();
+            users.setNrRoli(numerRoli);
+        }
+        model.addAttribute("userList", userList);
+        return "admin/usersearch";
+    }
+
+
+    // Pobranie listy userów
+    private Page<User> getAllUsersPageable(int page) {
+        Page<User> pages = adminService.findAll(PageRequest.of(page, ELEMENTS));
+        for (User users : pages) {
+            int numerRoli = users.getRoles().iterator().next().getId();
+            users.setNrRoli(numerRoli);
+        }
+        return pages;
+    }
+
+    // przygotowanie mapy ról
+    public Map<Integer, String> prepareRoleMap() {
+        Locale locale = Locale.getDefault();
+        Map<Integer, String> roleMap = new HashMap<Integer, String>();
+        roleMap.put(1, messageSource.getMessage("word.admin", null, locale));
+        roleMap.put(2, messageSource.getMessage("word.user", null, locale));
+        return roleMap;
+    }
+
+    // przygotowanie may aktywny/nieaktywny
+    public Map<Integer, String> prepareActivityMap() {
+        Locale locale = Locale.getDefault();
+        Map<Integer, String> activityMap = new HashMap<Integer, String>();
+        activityMap.put(0, messageSource.getMessage("word.nie", null, locale));
+        activityMap.put(1, messageSource.getMessage("word.tak", null, locale));
+        return activityMap;
+    }
 }
