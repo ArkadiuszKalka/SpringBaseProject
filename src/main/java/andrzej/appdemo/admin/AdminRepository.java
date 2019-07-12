c0772e1e033ff4aa26ec24b4f2738a5a4442c67b
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-12 14:41:15 +0200

    Dodano query findAllSearch do AdminRepository

--- src/main/java/andrzej/appdemo/admin/AdminRepository.java
+++ src/main/java/andrzej/appdemo/admin/AdminRepository.java
@@ -8,16 +8,21 @@ import org.springframework.stereotype.Repository;
 
 import andrzej.appdemo.user.User;
 
+import java.util.List;
+
 @Repository("adminRepository")
 public interface AdminRepository extends JpaRepository<User, Integer> {
 
-	User findUserById(int id);
+    User findUserById(int id);
 
-	@Modifying
-	@Query("UPDATE User u SET u.active = :intActive WHERE u.id= :id")
-	void updateActivationUser(@Param("intActive")int active, @Param("id") int id);
-	
-	@Modifying
-	@Query(value = "UPDATE user_role r SET r.role_id = :roleId WHERE r.user_id= :id", nativeQuery = true)
-	void updateRoleUser(@Param("roleId") int nrRoli, @Param("id") int id);
+    @Modifying
+    @Query("UPDATE User u SET u.active = :intActive WHERE u.id= :id")
+    void updateActivationUser(@Param("intActive") int active, @Param("id") int id);
+
+    @Modifying
+    @Query(value = "UPDATE user_role r SET r.role_id = :roleId WHERE r.user_id= :id", nativeQuery = true)
+    void updateRoleUser(@Param("roleId") int nrRoli, @Param("id") int id);
+
+    @Query(value = "SELECT * from User u where  u.name like %:param% or u.last_name like %:param% or u.email like %:param%", nativeQuery = true)
+    List<User> findAllSearch(@Param("param") String param);
 }
