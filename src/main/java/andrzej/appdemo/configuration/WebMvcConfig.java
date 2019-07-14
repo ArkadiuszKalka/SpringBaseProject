578084b18470df962a93c89b906866ef0620a413
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-14 14:41:11 +0200

    Dodano do WebMvcConfig zapis zwiększający max wielkośc pliku obsługiwana przez Tomcat

--- src/main/java/andrzej/appdemo/configuration/WebMvcConfig.java
+++ src/main/java/andrzej/appdemo/configuration/WebMvcConfig.java
@@ -1,9 +1,12 @@
 package andrzej.appdemo.configuration;
 
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.MultipartConfigElement;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -14,4 +17,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		return bcp;
 	}
 
+	@Bean
+	public MultipartConfigElement multipartConfigElement(){
+		MultipartConfigFactory factory=new MultipartConfigFactory();
+		factory.setMaxFileSize("5MB");
+		factory.setMaxRequestSize("5MB");
+		return factory.createMultipartConfig();
+	}
 }
