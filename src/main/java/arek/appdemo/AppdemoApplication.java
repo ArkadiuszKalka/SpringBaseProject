20077efb2a305a918bb49ae36531c40c97d61657
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-16 12:48:01 +0200

    Refaktoryzacja pakietów

---
+++ src/main/java/arek/appdemo/AppdemoApplication.java
@@ -0,0 +1,13 @@
+package arek.appdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AppdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppdemoApplication.class, args);
+	}
+}
---
+++ src/main/java/arek/appdemo/user/UserService.java
@@ -0,0 +1,10 @@
+package arek.appdemo.user;
+
+public interface UserService {
+	
+	public User findUserByEmail(String email);
+	public void saveUser(User user);
+	public void updateUserPassword(String newPassword, String email);
+	public void updateUserProfile(String newName, String newLastName, String newEmail, int id);
+	
+}
