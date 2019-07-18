fdd25c3e387610407ffd465ceecdf19ffb0e6703
Arkadiusz Kałka <arkadiusz.kalka@gmail.com>
2019-07-18 12:27:00 +0200

    Dodano logowanie rejestracji nowego usera w RegisterController

--- src/main/java/arek/appdemo/user/RegisterController.java
+++ src/main/java/arek/appdemo/user/RegisterController.java
@@ -6,6 +6,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 
 import arek.appdemo.validators.UserRegisterValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class RegisterController {
+
+	private static final Logger LOG= LoggerFactory.getLogger(RegisterController.class);
 	
 	@Autowired
 	private UserService userService;
@@ -33,9 +37,9 @@ public class RegisterController {
 	@POST
 	@RequestMapping(value = "/adduser")
 	public String registerAction(User user, BindingResult result, Model model, Locale locale) {
-				
+
 		String returnPage = null;
-		
+
 		User userExist = userService.findUserByEmail(user.getEmail());
 		
 		new UserRegisterValidator().validateEmailExist(userExist, result);
@@ -46,6 +50,7 @@ public class RegisterController {
 			returnPage = "register";
 		} else {
 			userService.saveUser(user);
+			LOG.info("**** UTWORZONO USERA( "+user.getName());
 			model.addAttribute("message", messageSource.getMessage("user.register.success", null, locale));
 			model.addAttribute("user", new User());
 			returnPage = "register";
